0971e6ef2a1ca295a9d7bfded91916b6f6e1f22b
Taras Mankovski <tarasm@gmail.com>
2017-08-15 11:53:06 +0530

    Added use and merge operations

--- __tests__/directory.test.ts
+++ __tests__/directory.test.ts
@@ -1,170 +1,218 @@
 import Directory from '../lib/directory';
-import Fixturify = require('broccoli-fixturify');
-import broccoli = require('broccoli');
-import fixturify = require('fixturify');
+import * as Fixturify from 'broccoli-fixturify';
+import * as broccoli from 'broccoli';
+import * as fixturify from 'fixturify';
+import * as Filter from 'broccoli-filter';
 
-let fixtures;
-beforeEach(() => {
-  fixtures = new Fixturify({
-    'a.txt': 'A',
-    'b.txt': 'B',
-    c: {
-      'd.txt': 'D'
+describe('directory', () => {
+  let fixtures;
+  beforeEach(() => {
+    fixtures = new Fixturify({
+      'a.txt': 'A',
+      'b.txt': 'B',
+      c: {
+        'd.txt': 'D',
+      },
+    });
+  });
+
+  let builder;
+  afterEach(() => {
+    if (builder) {
+      builder.cleanup();
     }
   });
-});
 
-let builder;
-afterEach(() => {
-  if (builder) {
-    builder.cleanup();
-  }
-});
+  describe('instantiation', function() {
+    let result;
+    beforeEach(() => {
+      let dir = new Directory('__tests__/fixtures/testdir');
+      builder = new broccoli.Builder(dir);
 
-describe('instantiation', function() {
-  let result;
-  beforeEach(() => {
-    let dir = new Directory('__tests__/fixtures/testdir');
-    builder = new broccoli.Builder(dir);
-
-    return builder.build()
-      .then(() => {
+      return builder.build().then(() => {
         result = fixturify.readSync(builder.outputPath);
       });
-  });
+    });
 
-  test('from string path', () => {
-    expect(result).toEqual({
-      'taras.txt': 'taras was here'
+    test('from string path', () => {
+      expect(result).toEqual({
+        'taras.txt': 'taras was here',
+      });
     });
   });
 
-});
+  describe('map', () => {
+    let result;
+    beforeEach(() => {
+      let dir = new Directory(fixtures);
 
-describe('map', () => {
+      builder = new broccoli.Builder(dir.map(content => `<a>${content}</a>`));
 
-  let result;
-  beforeEach(() => {
-    let dir = new Directory(fixtures);
-
-    builder = new broccoli.Builder(
-      dir
-        .map(content => `<a>${content}</a>`)
-    )
-
-    return builder.build()
-      .then(() => {
+      return builder.build().then(() => {
         result = fixturify.readSync(builder.outputPath);
       });
+    });
+
+    test('map invokes callback on each object', () => {
+      expect(result).toEqual({
+        'a.txt': '<a>A</a>',
+        'b.txt': '<a>B</a>',
+        c: {
+          'd.txt': '<a>D</a>',
+        },
+      });
+    });
   });
 
-  test('map invokes callback on each object', () => {
-    expect(result).toEqual({
-      'a.txt': '<a>A</a>',
-      'b.txt': '<a>B</a>',
-      c: {
-        'd.txt': '<a>D</a>'
+  describe('filter', () => {
+    let result;
+    beforeEach(() => {
+      let dir = new Directory(fixtures);
+
+      builder = new broccoli.Builder(dir.filter(content => content !== 'B'));
+
+      return builder.build().then(() => {
+        result = fixturify.readSync(builder.outputPath);
+      });
+    });
+
+    test('filter removes objects that return false', () => {
+      expect(result).toEqual({
+        'a.txt': 'A',
+        c: {
+          'd.txt': 'D',
+        },
+      });
+    });
+  });
+
+  describe('reduce', () => {
+    let result;
+    beforeEach(() => {
+      let fixtures = new Fixturify({
+        'a.txt': 'a',
+        'b.txt': 'b',
+        'c.txt': 'c',
+        'd.txt': 'd',
+      });
+
+      let dir = new Directory(fixtures);
+
+      builder = new broccoli.Builder(
+        dir.reduce(
+          (accumulator, content) => accumulator + content,
+          '',
+          'result.txt',
+        ),
+      );
+
+      return builder.build().then(() => {
+        result = fixturify.readSync(builder.outputPath);
+      });
+    });
+
+    test('reduces to a single file', function() {
+      expect(result).toEqual({
+        'result.txt': 'abcd',
+      });
+    });
+  });
+
+  describe('composition', () => {
+    let result;
+
+    beforeEach(() => {
+      let fixtures = new Fixturify({
+        'a.txt': '   a   ',
+        'b.txt': 'b',
+        'c.txt': '      c',
+        'd.txt': 'd',
+      });
+
+      let dir = new Directory(fixtures);
+
+      builder = new broccoli.Builder(
+        dir
+          .map(text => text.replace(/\s/g, ''))
+          .filter(text => text !== 'c')
+          .reduce((accumulator, text) => accumulator + text, '', 'result.txt'),
+      );
+
+      return builder.build().then(() => {
+        result = fixturify.readSync(builder.outputPath);
+      });
+    });
+
+    test('all of the filters were applied', function() {
+      expect(result).not.toEqual({
+        'result.txt': 'abcd',
+      });
+      expect(result).toEqual({
+        'result.txt': 'abd',
+      });
+    });
+  });
+
+  describe('merge', () => {
+    let result;
+
+    beforeEach(() => {
+      let a = new Fixturify({
+        'a.txt': 'a',
+      });
+
+      let b = new Fixturify({
+        'b.txt': 'b',
+      });
+
+      let $da = new Directory(a);
+      let $db = new Directory(b);
+
+      builder = new broccoli.Builder($da.merge(() => $db));
+
+      return builder.build().then(() => {
+        result = fixturify.readSync(builder.outputPath);
+      });
+    });
+
+    test('all of the filters were applied', function() {
+      expect(result).toEqual({
+        'a.txt': 'a',
+        'b.txt': 'b',
+      });
+    });
+  });
+
+  describe('use', () => {
+    let result;
+
+    beforeEach(() => {
+      let a = new Fixturify({
+        'a.txt': 'a',
+      });
+
+      class Emphasize extends Filter {
+        constructor(inputNode) {
+          super(inputNode);
+        }
+        processString(contents) {
+          return `*${contents}*`;
+        }
       }
-    });
-  });
 
-});
+      let $d = new Directory(a);
 
-describe('filter', () => {
+      builder = new broccoli.Builder($d.use(tree => new Emphasize(tree)));
 
-  let result;
-  beforeEach(() => {
-    let dir = new Directory(fixtures);
-
-    builder = new broccoli.Builder(
-      dir
-        .filter(content => content !== 'B')
-    )
-
-    return builder.build()
-      .then(() => {
+      return builder.build().then(() => {
         result = fixturify.readSync(builder.outputPath);
       });
-  });
-
-  test('filter removes objects that return false', () => {
-    expect(result).toEqual({
-      'a.txt': 'A',
-      c: {
-        'd.txt': 'D'
-      }
-    });
-  });
-
-});
-
-describe('reduce', () => {
-
-  let result;
-  beforeEach(() => {
-    let fixtures = new Fixturify({
-      'a.txt': 'a',
-      'b.txt': 'b',
-      'c.txt': 'c',
-      'd.txt': 'd'
     });
 
-    let dir = new Directory(fixtures);
-
-    builder = new broccoli.Builder(
-      dir
-        .reduce((accumulator, content) => accumulator + content, '', 'result.txt') 
-    )
-
-    return builder.build()
-      .then(() => {
-        result = fixturify.readSync(builder.outputPath);
+    test('all of the filters were applied', function() {
+      expect(result).toEqual({
+        'a.txt': '*a*',
       });
-  });
-
-  test('reduces to a single file', function() {
-    expect(result).toEqual({
-      'result.txt': 'abcd'
     });
   });
-
-});
-
-describe('composition', () => {
-
-  let result;
-
-  beforeEach(() => {
-    let fixtures = new Fixturify({
-      'a.txt': '   a   ',
-      'b.txt': 'b',
-      'c.txt': '      c',
-      'd.txt': 'd'
-    });
-
-  let dir = new Directory(fixtures);
-
-    builder = new broccoli.Builder(
-      dir
-        .map(text => text.replace(/\s/g,''))
-        .filter(text => text !== 'c')
-        .reduce((accumulator, text) => accumulator + text, '', 'result.txt')
-        
-    );
-
-    return builder.build()
-      .then(() => {
-        result = fixturify.readSync(builder.outputPath);
-      });
-  });
-
-  test('all of the filters were applied', function() {
-    expect(result).not.toEqual({
-      'result.txt': 'abcd'
-    });
-    expect(result).toEqual({
-      'result.txt': 'abd'
-    });
-  });
-
 });
--- lib/directory.ts
+++ lib/directory.ts
@@ -1,10 +1,13 @@
 import ReduceFilter from './filters/reduce';
 import MapFilter from './filters/map';
 import FilterFilter from './filters/filter';
-import Plugin = require('broccoli-plugin');
-import Funnel = require('broccoli-funnel');
-import broccoli = require('broccoli');
-import copyDereference = require('copy-dereference');
+import * as Plugin from 'broccoli-plugin';
+import * as Funnel from 'broccoli-funnel';
+import * as broccoli from 'broccoli';
+import * as copyDereference from 'copy-dereference';
+import * as BroccoliMergeTrees from 'broccoli-merge-trees';
+import * as BroccoliStew from 'broccoli-stew';
+
 export default class Directory {
   private node;
 
@@ -27,42 +30,59 @@ export default class Directory {
   __broccoliFeatures__ = Plugin.prototype.__broccoliFeatures__;
 
   __broccoliGetInfo__(builderFeatures) {
-    return Plugin.prototype.__broccoliGetInfo__.bind(this.node)(builderFeatures);
+    return Plugin.prototype.__broccoliGetInfo__.bind(this.node)(
+      builderFeatures,
+    );
   }
 
   map(callback: (content: string, relativePath: string) => any) {
-    
     let node = new MapFilter(this.node, {
-      callback
+      callback,
     });
 
     return new Directory(node);
   }
 
   filter(callback: (content: string, relativePath: string) => any) {
-
     let node = new FilterFilter(this.node, {
-      callback
+      callback,
     });
 
     return new Directory(node);
   }
 
-  reduce(callback: (accumulator: any, value: any, relativePath: string) => any, initialValue: any, outputFile: string) {
-
+  reduce(
+    callback: (accumulator: any, value: any, relativePath: string) => any,
+    initialValue: any,
+    outputFile: string,
+  ) {
     let node = new ReduceFilter(this.node, {
       callback,
       initialValue,
-      outputFile
+      outputFile,
     });
 
+    return new Directory(node);
+  }
+
+  use(callback: (tree) => any) {
+    const node = callback(this.node);
+    return new Directory(node);
+  }
+
+  log(options = { output: 'tree' }) {
+    const node = BroccoliStew.log(this.node, options);
+    return new Directory(node);
+  }
+
+  merge(callback: (tree) => any) {
+    const node = new BroccoliMergeTrees([this.node, callback(this.node)]);
     return new Directory(node);
   }
 
   build(outputDir) {
     let builder = new broccoli.Builder(this);
-    let promise = builder.build()
-      .then(() => builder);
+    let promise = builder.build().then(() => builder);
 
     if (outputDir) {
       promise.then(builder => {
